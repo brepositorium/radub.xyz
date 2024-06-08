945c6784b31075320aebac91fd99b934df613f6a
brepositorium <rcryptob@protonmail.ch>
2024-06-08 12:59:40 +0300

    add dependencies and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
 module.exports = {
   content: [
-    './_drafts/**/*.html',
-    './_includes/**/*.html',
-    './_layouts/**/*.html',
-    './_posts/*.md',
-    './*.markdown',
-    './*.html',
+    "./_drafts/**/*.html",
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/*.md",
+    "./*.markdown",
+    "./*.html",
   ],
   theme: {
     extend: {
       blur: {
-        xs: '2px',
+        xs: "2px",
       },
       boxShadow: {
-        bigInner: 'inset 0 10px 15px -3px rgba(0, 0, 0, 0.1), inset 0 4px 6px -2px rgba(0, 0, 0, 0.25)'
-      }
+        bigInner:
+          "inset 0 10px 15px -3px rgba(0, 0, 0, 0.1), inset 0 4px 6px -2px rgba(0, 0, 0, 0.25)",
+      },
+      fontFamily: {
+        poppins: ["poppins", "sans-serif"]
+      },
     },
   },
-  plugins: []
-}
+  plugins: [],
+};
